72a6c725c62feae3e216842cd7ce0f3d2ce80a2e
Nurliah Awaliah <nurliaha@gmail.com>
2023-11-24 13:25:55 +0700

    change describe

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,4 +1,4 @@
-describe("Access form test qa", function(){
+describe("Positif - As a user, I can submit form Review Product ", function(){
   it("verify Access link test qa", function() {
     cy.visit("https://forms.office.com/pages/responsepage.aspx?id=is2XW8LLaEmfFhLKD9VwE9lpKmxdveNGmMWKETZvAWNUMzhBV1lYTlc1SDNRS00xRVg4OFhPODlQTS4u") 
     cy.title().should('eq','Review our product')
