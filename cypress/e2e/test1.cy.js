4526373caadaf75b8ac60e87b553cdf96a967173
Nurliah Awaliah <nurliaha@gmail.com>
2023-11-24 14:04:43 +0700

    add scenario valid submit form

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,8 +1,21 @@
 describe("Positif - As a user, I can submit form Review Product ", function(){
 
-  it("Verivy access form review product", function() {
+  it("Verify access form review product", function() {
     cy.visit("https://forms.office.com/pages/responsepage.aspx?id=is2XW8LLaEmfFhLKD9VwE9lpKmxdveNGmMWKETZvAWNUMzhBV1lYTlc1SDNRS00xRVg4OFhPODlQTS4u") 
 
+  })
+
+  it("verify valid input for submit form", function(){
+    cy.visit("https://forms.office.com/pages/responsepage.aspx?id=is2XW8LLaEmfFhLKD9VwE9lpKmxdveNGmMWKETZvAWNUMzhBV1lYTlc1SDNRS00xRVg4OFhPODlQTS4u")
+    cy.get('input[data-automation-id="textInput"]').should('have.length', 3) 
+    cy.get('input[data-automation-id="textInput"]').eq(0).type("Nana")
+    cy.get('input[data-automation-id="textInput"]').eq(1).type('098766554462')
+    cy.get('input[data-automation-id="textInput"]').eq(2).type("Nanananana")
+    cy.get('[aria-label="3 Star"]').click()
+    cy.get("#DatePicker0-label").click()
+    cy.get('[aria-label="23, November, 2023"]').click()
+    cy.get("[data-automation-id='submitButton']").click()
+    cy.get("[data-automation-id='saveAndEditButton']").should('be.visible')
   })
 
   it("verify report abuse", function(){
