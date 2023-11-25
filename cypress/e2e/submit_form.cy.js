0c3b6168159da002478a80c7ef3147b0ba491d8d
Nurliah Awaliah <nurliaha@gmail.com>
2023-11-25 22:01:59 +0700

    fix lint

--- cypress/e2e/submit_form.cy.js
+++ cypress/e2e/submit_form.cy.js
@@ -17,18 +17,17 @@ describe("Positif - As a user, I can submit form Review Product ", function(){
   })
 
   it("verify submit form report abuse", function(){
-      cy.get('a:contains("Report abuse")').click()
-      cy.get('[data-automation-value="reportAbuseViolationRadioGroup"]').should('have.length',3)
-      cy.get("#Phishing").check()
-      cy.get('span:contains("Copyright infringement")').should("be.visible")
-      cy.get("#Other").check()
-      cy.get("#Phishing").click()
-      cy.get('[aria-labelledby="ReportAbuseId_CommentsTitle"]').type("Get all following siblings of each DOM element in a set of matched DOM elements up to, but not including, the element provided.")
-      cy.get('button:contains("Submit")').should("be.visible")
-      cy.get('button:contains("Submit")').click()
-      cy.get('div:contains("Your report was submitted.")').should("contain.text", "Your report was submitted.")
-      
-    })
+    cy.get('a:contains("Report abuse")').click()
+    cy.get('[data-automation-value="reportAbuseViolationRadioGroup"]').should('have.length', 3)
+    cy.get("#Phishing").check()
+    cy.get('span:contains("Copyright infringement")').should("be.visible")
+    cy.get("#Other").check()
+    cy.get("#Phishing").click()
+    cy.get('[aria-labelledby="ReportAbuseId_CommentsTitle"]').type("Get all following siblings of each DOM element in a set of matched DOM elements up to, but not including, the element provided.")
+    cy.get('button:contains("Submit")').should("be.visible")
+    cy.get('button:contains("Submit")').click()
+    cy.get('div:contains("Your report was submitted.")').should("contain.text", "Your report was submitted.")
+  })
 }) 
 
 describe("Negative - As a user, I cannot submit form Review Product ", function(){
@@ -36,6 +35,7 @@ describe("Negative - As a user, I cannot submit form Review Product ", function(
     cy.visit("https://forms.office.com/pages/responsepage.aspx?id=is2XW8LLaEmfFhLKD9VwE9lpKmxdveNGmMWKETZvAWNUMzhBV1lYTlc1SDNRS00xRVg4OFhPODlQTS4u")
     cy.get('title:contains("Review our product")').should("contain.text", "Review our product")
   })
+
   it("verify can't submit when field name empty", function(){
     cy.get('input[data-automation-id="textInput"]').should('have.length', 3) 
     cy.get('input[data-automation-id="textInput"]').eq(0).invoke('val', '')
@@ -72,6 +72,7 @@ describe("Positive - As a user, I can verify clear form ", function(){
     cy.visit("https://forms.office.com/pages/responsepage.aspx?id=is2XW8LLaEmfFhLKD9VwE9lpKmxdveNGmMWKETZvAWNUMzhBV1lYTlc1SDNRS00xRVg4OFhPODlQTS4u")
     cy.get('title:contains("Review our product")').should("contain.text", "Review our product")
   })
+
   it("As a user, I can clearing form permanently erase any information have entered", function(){
     cy.get('input[data-automation-id="textInput"]').eq(0).type("Nana")
     cy.get('input[data-automation-id="textInput"]').eq(1).type('098766554462')
@@ -95,7 +96,6 @@ describe("Positive - As a user, I can verify clear form ", function(){
     cy.get('[data-automation-id="dateContainer"] > div > div').should("not.have.class","is-active")
     
   })
-
 
   it("verify cancelled clearing form permanently ", function(){
     cy.get('input[data-automation-id="textInput"]').should('have.length', 3) 
@@ -132,7 +132,6 @@ describe("Positive - As a user, I can verify clear form ", function(){
     cy.get('input[data-automation-id="textInput"]').eq(0).should("have.value", "Nana")
     cy.get('input[data-automation-id="textInput"]').eq(1).should("have.value", "098766554462")
     cy.get('input[data-automation-id="textInput"]').eq(2).should("have.value", "Nanananana")
-    
   })
 })
 
@@ -141,6 +140,7 @@ describe("Positive - verify to Enable or Disable Immersive Reader", function(){
     cy.visit("https://forms.office.com/pages/responsepage.aspx?id=is2XW8LLaEmfFhLKD9VwE9lpKmxdveNGmMWKETZvAWNUMzhBV1lYTlc1SDNRS00xRVg4OFhPODlQTS4u")
     cy.get('title:contains("Review our product")').should("contain.text", "Review our product")
   })
+
   it("As a user, I can Enable immersicer reader in form ", function(){
     cy.get('[aria-label="More options"]').click()
     cy.get('[role="menuitem"]').should("have.length", 2)
@@ -153,7 +153,6 @@ describe("Positive - verify to Enable or Disable Immersive Reader", function(){
     cy.get('[aria-label="Immersive Reader"]').eq(3).should("be.enabled")
     cy.get('[aria-label="Immersive Reader"]').eq(4).should("be.enabled")
     cy.get('[aria-label="Immersive Reader"]').eq(5).should("be.enabled")
-
   })
 
   it("As a user, I can Disable immersicer reader in form", function(){
@@ -164,12 +163,12 @@ describe("Positive - verify to Enable or Disable Immersive Reader", function(){
     cy.get('span:contains("Enable Immersive Reader")').click()
     //assertion enable
     cy.get('[aria-label="Immersive Reader"]').should("have.length", 6)
-  cy.get('[aria-label="Immersive Reader"]').eq(0).should("be.enabled")
-  cy.get('[aria-label="Immersive Reader"]').eq(1).should("be.enabled")
-  cy.get('[aria-label="Immersive Reader"]').eq(2).should("be.enabled")
-  cy.get('[aria-label="Immersive Reader"]').eq(3).should("be.enabled")
-  cy.get('[aria-label="Immersive Reader"]').eq(4).should("be.enabled")
-  cy.get('[aria-label="Immersive Reader"]').eq(5).should("be.enabled")
+    cy.get('[aria-label="Immersive Reader"]').eq(0).should("be.enabled")
+    cy.get('[aria-label="Immersive Reader"]').eq(1).should("be.enabled")
+    cy.get('[aria-label="Immersive Reader"]').eq(2).should("be.enabled")
+    cy.get('[aria-label="Immersive Reader"]').eq(3).should("be.enabled")
+    cy.get('[aria-label="Immersive Reader"]').eq(4).should("be.enabled")
+    cy.get('[aria-label="Immersive Reader"]').eq(5).should("be.enabled")
 
     cy.get('[aria-label="More options"]').click()
     cy.get('[role="menuitem"]').should("have.length", 2)
